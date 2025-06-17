0e5af718c40e78526c9b9be5d7c388ff56b34622
StouneRocco <dijonnais376@gmail.com>
2025-06-17 10:47:26 +0200

    .

--- tes2.py
+++ tes2.py
@@ -12,7 +12,7 @@ from methodeCoude import *
 from sklearn.preprocessing import StandardScaler
 from sklearn.cluster import KMeans
 
-from regression import regression_lineaire
+from regression import regression_lineaire, regression_sklearn_simple
 
 # Chargement multi-départements
 files = {
